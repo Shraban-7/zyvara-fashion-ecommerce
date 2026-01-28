07feda3c81fab1cf4c20a2b4f9055bf6e7330205
Shraban-7 <shakuatshraban@gmail.com>
2026-01-28 16:18:07 +0600

    seeder update

--- database/seeders/BannerSeeder.php
+++ database/seeders/BannerSeeder.php
@@ -10,38 +10,38 @@ class BannerSeeder extends Seeder
 {
     public function run(): void
     {
-        // 1. Generate 3 Hero Sliders (Left Side)
-        // $heroSliders = [
-        //     ['image' => 'banners/left1.png', 'order' => 1],
-        //     ['image' => 'banners/left2.png', 'order' => 2],
-        //     ['image' => 'banners/left3.png', 'order' => 3],
-        // ];
+        //1. Generate 3 Hero Sliders (Left Side)
+        $heroSliders = [
+            ['image' => 'banners/left1.png', 'order' => 1],
+            ['image' => 'banners/left2.png', 'order' => 2],
+            ['image' => 'banners/left3.png', 'order' => 3],
+        ];
 
-        // foreach ($heroSliders as $slider) {
-        //     Banner::create([
-        //         'title' => 'Hero Slider ' . $slider['order'],
-        //         'image' => $slider['image'],
-        //         'position' => BannerPosition::HERO->value,
-        //         'sort_order' => $slider['order'],
-        //         'is_active' => true,
-        //     ]);
-        // }
+        foreach ($heroSliders as $slider) {
+            Banner::create([
+                'title' => 'Hero Slider ' . $slider['order'],
+                'image' => $slider['image'],
+                'position' => BannerPosition::HERO->value,
+                'sort_order' => $slider['order'],
+                'is_active' => true,
+            ]);
+        }
 
-        // // 2. Generate 2 Promotional Banners (Right Side)
-        // $promoBanners = [
-        //     ['image' => 'banners/right1.png', 'order' => 1],
-        //     ['image' => 'banners/right2.png', 'order' => 2],
-        // ];
+        // 2. Generate 2 Promotional Banners (Right Side)
+        $promoBanners = [
+            ['image' => 'banners/right1.png', 'order' => 1],
+            ['image' => 'banners/right2.png', 'order' => 2],
+        ];
 
-        // foreach ($promoBanners as $promo) {
-        //     Banner::create([
-        //         'title' => 'Right Promo ' . $promo['order'],
-        //         'image' => $promo['image'],
-        //         'position' => BannerPosition::PROMOTIONAL->value,
-        //         'sort_order' => $promo['order'],
-        //         'is_active'  => true,
-        //     ]);
-        // }
+        foreach ($promoBanners as $promo) {
+            Banner::create([
+                'title' => 'Right Promo ' . $promo['order'],
+                'image' => $promo['image'],
+                'position' => BannerPosition::PROMOTIONAL->value,
+                'sort_order' => $promo['order'],
+                'is_active'  => true,
+            ]);
+        }
 
         //festival banner
 
