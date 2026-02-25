bd76f43ce2592466ae487c373d7da44dc4064e4f
Shraban-7 <shakuatshraban@gmail.com>
2026-02-25 14:50:20 +0600

    components updated

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -2,7 +2,7 @@
 
 <div class="w-full">
     @if($label)
-        <label for="{{ $name }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        <label for="{{ $name }}" class="block mb-2 text-sm font-medium text-gray-900 ">
             {{ $label }}
         </label>
     @endif
@@ -13,10 +13,10 @@
         {{ $disabled ? 'disabled' : '' }}
         {!! $attributes->merge([
             'class' => 'w-full px-4 py-2.5 rounded-lg
-            bg-white dark:bg-gray-800
-            border border-gray-200 dark:border-gray-700
-            text-gray-900 dark:text-white
-            placeholder-gray-400 dark:placeholder-gray-500
+            bg-white
+            border border-gray-200
+            text-gray-900
+            placeholder-gray-400 
             transition duration-200 ease-in-out
             focus:outline-none
             focus:ring-2 focus:ring-blue-500
--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -11,10 +11,10 @@
         rows="{{ $rows }}" 
         placeholder="{{ $placeholder }}"
         {{ $attributes->merge(['class' => 'w-full px-4 py-2.5 rounded-lg
-            bg-white dark:bg-gray-800
-            border border-gray-200 dark:border-gray-700
-            text-gray-900 dark:text-white
-            placeholder-gray-400 dark:placeholder-gray-500
+            bg-white 
+            border border-gray-200 
+            text-gray-900 
+            placeholder-gray-400
             transition duration-200 ease-in-out
             focus:outline-none
             focus:ring-2 focus:ring-blue-500
