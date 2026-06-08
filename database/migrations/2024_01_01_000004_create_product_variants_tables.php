cac6be5af3e501edfe9f240187f95b31d499d7fc
Shraban-7 <shakuatshraban@gmail.com>
2026-06-08 16:57:22 +0600

    migration

--- database/migrations/2024_01_01_000004_create_product_variants_tables.php
+++ database/migrations/2024_01_01_000004_create_product_variants_tables.php
@@ -24,7 +24,7 @@ return new class extends Migration
         Schema::create('colors', function (Blueprint $table) {
             $table->id();
             $table->string('name'); // White, Black, Blue, etc.
-            $table->string('code'); // white, black, blue
+            $table->string('code')->nullable(); // white, black, blue
             $table->string('hex_code')->nullable(); // #FFFFFF
             $table->timestamps();
         });
